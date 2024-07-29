269e9dc5d600e46b035f69d229824c01c89d6e7b
mkaufman385 <mkaufman385@gmail.com>
2024-07-29 13:06:09 -0400

    wip. hamburger menu button added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../styles/Navbar.css";
+import Reorder from "@material-ui/icons/Reorder";
+// import Reorder from '@m'
 
 function Navbar() {
   return (
     <div className="navbar">
       <div className="toggleButton">
-        <button></button>
+        <button>
+          <Reorder />
+        </button>
       </div>
       <div className="links">
         <Link to="/">Home</Link>
