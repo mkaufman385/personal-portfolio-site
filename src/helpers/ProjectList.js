f915b1cb68407e941a23493a28d89ccbc5eb5b88
mkaufman385 <mkaufman385@gmail.com>
2024-07-31 10:34:46 -0400

    wip. projects page--> project title and image icon showing properly with map function. not clickable yet

---
+++ src/helpers/ProjectList.js
@@ -0,0 +1,13 @@
+import Proj1 from "../assets (images)/Personal-portfolio-site-image.png";
+//  IMPORT ALL NEEDED PROJECT IMAGES
+
+export const projectList = [
+  {
+    name: "Personal Portfolio Website",
+    image: Proj1,
+  },
+  // {
+  //   name: "",
+  //   image: ,
+  // },
+];
