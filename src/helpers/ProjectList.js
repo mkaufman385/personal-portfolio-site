f5a091f67c2f7641aea083f06842a894639a701c
mkaufman385 <mkaufman385@gmail.com>
2024-08-02 12:33:06 -0400

    wip. Projects page. Need to fix routes next

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -1,13 +1,23 @@
 import Proj1 from "../assets (images)/Personal-portfolio-site-image.png";
+import Proj2 from "../assets (images)/Asylum-office-grant-rate-tracker-site-image.png";
+import Proj3 from "../assets (images)/advanced-state-wheel-quiz-form-site-image.png";
 //  IMPORT ALL NEEDED PROJECT IMAGES
 
 export const projectList = [
   {
     name: "Personal Portfolio Website",
     image: Proj1,
+    skills: "INSERT SKILLS HERE",
   },
-  // {
-  //   name: "",
-  //   image: ,
-  // },
+  {
+    name: "Asylum Office Grant Rate Tracker",
+    image: Proj2,
+    skills: "INSERT SKILLS HERE",
+  },
+
+  {
+    name: "Wheel, Quiz, Form",
+    image: Proj3,
+    skills: "INSERT SKILLS HERE",
+  },
 ];
