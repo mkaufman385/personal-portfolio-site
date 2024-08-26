2b50bb2fa9f30238d1576cf3ce19376aab344565
mkaufman385 <mkaufman385@gmail.com>
2024-08-26 11:22:29 -0400

    wip. footer media query

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,10 @@
 import React from "react";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 
-// import GithubIcon from "@material-ui/icons/GithubIcon";
-// import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-// import { Link as ScrollLink } from "react-scroll";
 import "../styles/Footer.css";
 
 function Footer() {
+  const currentYear = new Date().getFullYear();
   return (
     <div className="footer">
       <div className="socialMedia">
@@ -19,7 +17,7 @@ function Footer() {
         </a>
       </div>
       <p>Created by Matthew Kaufman | mkaufman385@gmail.com</p>
-      <p>&copy; 2024 All Rights Reserved</p>
+      <p>&copy; {currentYear} All Rights Reserved</p>
     </div>
   );
 }
