0b36c85941484db1160469d2c08c52eb4eee3a6c
mkaufman385 <mkaufman385@gmail.com>
2024-08-26 16:53:52 -0400

    wip

--- src/vite.config.js
+++ src/vite.config.js
@@ -5,6 +5,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    historyApiFallback: true, // Enable fallback to index.html
+    historyApiFallback: true, // For client-side routing
   },
 });
